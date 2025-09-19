f37d1d1f613e61f14e20e48042ec817d04622523
Thabiso <thabiso@example.com>
2025-09-19 18:40:09 +0200

    feat: add example comment for error-handling branch

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 app.listen(PORT, () => {
   console.log(`Library API listening on http://localhost:${PORT}`);
 });
+"// Example comment for error-handling branch" 
